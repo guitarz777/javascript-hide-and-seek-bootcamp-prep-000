2dc8e895a468c560e2dd9e01f16f30281e9fa45d
guitarz777@gmail.com <guitarz777@gmail.com>
2018-09-07 17:40:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+
+function nestedTarget(){
+  return document.querySelector('#nested').querySelector('.target');
+}
+
+
+function increaseRankBy(n){
+  var a = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < a.length; i++){
+    a[i].innerHTML = parseInt(a[i].innerHTML, 10) + n ;
+  }
+}
+
+
+function deepestChild(){
+  var a = document.querySelector("#grand-node")
+  return a.lastChild
+  
+}
